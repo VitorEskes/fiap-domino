2fbb7688dc20d0bd14c0753ad07d168138475a8d
Vitor Eskes <vitoralveseskes@gmail.com>
2026-08-24 20:45:19 -0300

    feat(domain): configure Usuario and Jogador with data annotations

--- DominoPontaDeQuina.Domain/Entities/Jogador.cs
+++ DominoPontaDeQuina.Domain/Entities/Jogador.cs
@@ -1,10 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DominoPontaDeQuina.Domain.Entities;
 
+[Table("Jogadores")]
 public class Jogador
 {
+    [Key]
     public Guid Id { get; set; } = Guid.NewGuid();
+
+    [Required]
+    [MaxLength(80)]
     public string NomeExibicao { get; set; } = string.Empty;
+
+    [Required]
     public Guid UsuarioId { get; set; }
+
+    [ForeignKey(nameof(UsuarioId))]
+    [InverseProperty(nameof(Entities.Usuario.Jogadores))]
     public Usuario Usuario { get; set; } = null!;
+
+    [InverseProperty(nameof(ParticipacaoJogo.Jogador))]
     public ICollection<ParticipacaoJogo> Participacoes { get; set; } = new List<ParticipacaoJogo>();
 }
