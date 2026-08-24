112180bbbb15c7934e45f453298b61fed9ed5c3f
Vitor Eskes <vitoralveseskes@gmail.com>
2026-08-24 20:45:19 -0300

    feat(repository): implement DominoDbContext with dbsets and sqlite connection

--- DominoPontaDeQuina.Repository/Context/DominoDbContext.cs
+++ DominoPontaDeQuina.Repository/Context/DominoDbContext.cs
@@ -1,7 +1,25 @@
+using DominoPontaDeQuina.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace DominoPontaDeQuina.Repository.Context;
 
 public class DominoDbContext : DbContext
 {
+    private const string ConnectionString = "Data Source=domino.db";
+
+    public DbSet<Usuario> Usuarios { get; set; } = null!;
+
+    public DbSet<Jogador> Jogadores { get; set; } = null!;
+
+    public DbSet<Jogo> Jogos { get; set; } = null!;
+
+    public DbSet<ParticipacaoJogo> ParticipacoesJogo { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite(ConnectionString);
+        }
+    }
 }
